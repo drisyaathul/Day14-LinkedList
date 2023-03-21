2c5914f8da81ebc1049ba42a14dfaf14bc62ed38
drisyaathul <drisyak809@gmail.com>
2023-03-21 22:52:26 +0800

    Added: Simple LinkedList Of 56,30,70

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -5,14 +5,25 @@ public class LinkedList <T> {
     Node<T> head;
     Node<T> tail;
 
-    public void push(T data) {
+       public void add(T data) {
+
         Node<T> newNode = new Node<>(data);
         if (head == null) {
             head = newNode;
             tail = newNode;
         }else {
-            newNode.next = head;
-            head = newNode;
+            tail.next = newNode;
+            tail = newNode;
         }
+    }
+
+    public void display() {
+
+        Node<T> temp = head;
+        while (temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
 }
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -3,8 +3,11 @@ package com.bridgelabz;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList<Integer> linkedList1 = new LinkedList<>();
-        linkedList1.push(70);
-        linkedList1.push(30);
-        linkedList1.push(56);
+        linkedList1.add(56);
+        linkedList1.add(30);
+        linkedList1.add(70);
+
+        linkedList1.display();
+
     }
 }
