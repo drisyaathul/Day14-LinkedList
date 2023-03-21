b2b5f15c182a44d858934e22c6ad5e3bc340ebdc
drisyaathul <drisyak809@gmail.com>
2023-03-21 23:29:15 +0800

    Added: Creating LinkedList By Using Appending

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,17 +2,24 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        //uc1:
+
+        //UC1:
         LinkedList<Integer> linkedList1 = new LinkedList<>();
         linkedList1.add(56);
         linkedList1.add(30);
         linkedList1.add(70);
         linkedList1.display();
-        //uc2:
+        //UC2:
         LinkedList<Integer>linkedList2 = new LinkedList<>();
         linkedList2.push(70);
         linkedList2.push(30);
         linkedList2.push(56);
         linkedList2.display();
+        //UC3:
+        LinkedList<Integer>linkedList3 = new LinkedList<>();
+        linkedList3.append(56);
+        linkedList3.append(30);
+        linkedList3.append(70);
+        linkedList3.display();
     }
 }
